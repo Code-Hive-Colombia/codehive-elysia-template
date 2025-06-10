c2244d6059d1630e0882d9173c1b5bdd3b06994a
null-d-e-v <exception.nulldev@gmail.com>
2025-06-10 16:20:59 -0500

    🐛 (drizzle): Change drizzle config database env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/**/repository.ts",
+  schema: "./src/**/*.repository.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.PG_URL!,
   },
 });
